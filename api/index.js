5307be6aa7b3d9928fc253e2b65800bd5e8a260f
unpairedkrish-7 <krishnabhukya893@gmail.com>
2023-12-25 17:56:39 +0530

    create signup api  route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 dotenv.config();
+import userRouter from "./routers/user.route.js"
+import authRouter from "./routers/auth.router.js"
 const app = express()
 
 mongoose.connect(process.env.MONGO_SERVER).then(()=>{
@@ -9,6 +11,10 @@ mongoose.connect(process.env.MONGO_SERVER).then(()=>{
 }).catch((err)=>{
     console.log(err)
 })
+
+app.use(express.json());
+app.use("/api/user" , userRouter)
+app.use("/api/auth" , authRouter)
 
 
 app.listen(5000,()=>{
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,15 +5,15 @@ const userSchema = new mongoose.Schema({
     username : {
         type : String,
         required : true,
-        unique : true
+        unique : true,
     },
     email : {
         type : String,
         required : true,
-        unique : true
+        unique : true,
     },
     password : {
-        type : Number,
+        type : String,
         required : true,
     },
 
